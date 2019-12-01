fb549939cdf17365e8ca0c6445d06e816f3a71ee
Harish Saini <harish.saini@gwynniebee.com>
2019-12-01 18:26:51 +0530

    setup

--- server/app.py
+++ server/app.py
@@ -59,6 +59,7 @@ def start_server():
     webapp.api_update = BhavUpdateStocksWebService()
     cherrypy.tools.CORS = cherrypy.Tool("before_handler", CORS)
     cherrypy.config.update({
+        'server.socket_host': '0.0.0.0',
         'server.socket_port': int(os.environ.get('PORT', 3000)),
         'tools.CORS.on': True,
         'log.screen': True,
